01ad855906cd729a83017bbd0a1655e7966bf3d0
petebrowncorndel <peter.brown@corndel.com>
2024-06-15 15:19:33 +0100

    working index and find reviews for movie views

--- server/app.js
+++ server/app.js
@@ -1,7 +1,27 @@
 import express from 'express'
+import Movie from '../models/Movie.js'
 
 const app = express()
 
 // TODO: add your endpoints here
+
+app.set('view engine', 'ejs')
+
+app.get('/', async (req, res) => {
+    const movies = await Movie.findAll()
+    res.render('index', {
+        movies
+    })
+  })
+
+  app.get('/movie/:id', async (req, res) => {
+    const movie = await Movie.findById(req.params.id)
+    const reviews = await Movie.findReviews(req.params.id)
+    res.render('movie', {
+      movie,
+      reviews
+    })
+  })
+
 
 export default app
